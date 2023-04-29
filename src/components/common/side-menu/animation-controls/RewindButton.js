bce8614ee9ddaebce800e1bc5b1efc806e0ec986
Hagop Taminian <hagop.taminian@gmail.com>
2023-04-29 20:35:43 +0300

    fix: change undo button to rewind
    closes #129

--- src/components/common/side-menu/animation-controls/RewindButton.js
+++ src/components/common/side-menu/animation-controls/RewindButton.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import UndoIcon from '@material-ui/icons/Undo';
-import { teal } from '@material-ui/core/colors';
+import FastRewind from '@material-ui/icons/FastRewind';
+import { red } from '@material-ui/core/colors';
 import CustomButton from '../shared-components/CustomButton';
 import {
   clearPreviousSettings,
@@ -13,39 +13,39 @@ import {
 import { keepFluxesBlinking } from '../../../../utils';
 import { EMPTY_STRING } from '../../../../constants';
 
-const UndoButton = ({ className }) => {
+const RewindButton = ({ className }) => {
   const { t } = useTranslation();
   const { zoomedIn } = useSelector(({ layout }) => layout);
   const { previousSettings, animationPlaying } = useSelector(({ lab }) => lab);
   const dispatch = useDispatch();
 
-  const onUndo = () => {
+  const onRewind = () => {
     dispatch(setIsPaused(true));
     dispatch(restorePreviousSettings(previousSettings));
     keepFluxesBlinking(previousSettings.fluxesToBlink, dispatch);
     dispatch(clearPreviousSettings({}));
   };
 
-  const undoImpossible = !Object.keys(previousSettings).length;
+  const rewindImpossible = !Object.keys(previousSettings).length;
 
   return (
     <CustomButton
-      title={t('Undo')}
+      title={t('Rewind')}
       tooltipPlacement="top"
-      disabled={undoImpossible || zoomedIn || animationPlaying}
-      onClick={onUndo}
-      icon={<UndoIcon className={className} />}
+      disabled={rewindImpossible || zoomedIn || animationPlaying}
+      onClick={onRewind}
+      icon={<FastRewind className={className} />}
       color={
-        !undoImpossible && !animationPlaying && !zoomedIn
-          ? teal[900]
+        !rewindImpossible && !animationPlaying && !zoomedIn
+          ? red[800]
           : EMPTY_STRING
       }
     />
   );
 };
 
-UndoButton.propTypes = {
+RewindButton.propTypes = {
   className: PropTypes.string.isRequired,
 };
 
-export default UndoButton;
+export default RewindButton;
