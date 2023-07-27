3aa6285b0769681ed79df2f5660f21d3d1d1915a
Hagop Taminian <hagop.taminian@gmail.com>
2023-07-27 11:36:05 +0300

    feat: update function parameters, other constants
    closes #146

--- src/constants/physics.js
+++ src/constants/physics.js
@@ -2,7 +2,6 @@ import { ICE_AGE, MARS, TODAY, PRE_INDUSTRIAL, VENUS } from './strings';
 
 export const STEFAN_BOLTZMANN_CONSTANT = 5.670367e-8;
 export const ZERO_KELVIN_IN_CELISUS = 273.15;
-export const PRE_INDUSTRIAL_ALBEDO_OFFSET = -0.005;
 
 // above this temperature, the earth has heated so much that for all purposes there's no difference
 export const MAX_TEMPERATURE_DISPLAYED_ON_EARTH_CELSIUS = 30;
@@ -32,3 +31,8 @@ SOLAR_FLUXES[PRE_INDUSTRIAL] = 340;
 SOLAR_FLUXES[TODAY] = 340;
 SOLAR_FLUXES[MARS] = 147;
 SOLAR_FLUXES[VENUS] = 650;
+
+export const CRYOSPHERE_ALBEDO = 0.54;
+export const CLOUD_ALBEDO = 0.49;
+export const SOIL_ALBEDO_TODAY = 0.071;
+export const SOIL_ALBEDO_PREVIOUS_ERAS = 0.051;
