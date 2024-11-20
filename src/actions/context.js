31d9446acb788868bfdc111019366d504fdd3a86
Hagop Taminian <hagop.taminian@gmail.com>
2024-11-20 12:18:43 +0300

    feat: set radiation mode via query param
    closes #188

--- src/actions/context.js
+++ src/actions/context.js
@@ -3,9 +3,11 @@ import {
   FLAG_GETTING_CONTEXT,
   GET_CONTEXT_FAILED,
   GET_CONTEXT_SUCCEEDED,
+  SET_RADIATION_MODE,
 } from '../types';
 import { flag, getSettings } from './common';
 import { patchAppInstance } from './appInstance';
+import { RADIATION_MODES } from '../constants';
 
 // flags
 const flagGettingContext = flag(FLAG_GETTING_CONTEXT);
@@ -17,13 +19,17 @@ const flagGettingContext = flag(FLAG_GETTING_CONTEXT);
 const getContext = () => (dispatch) => {
   dispatch(flagGettingContext(true));
   try {
-    const { lang = 'en' } = Qs.parse(window.location.search, {
+    const { lang = 'en', radiation } = Qs.parse(window.location.search, {
       ignoreQueryPrefix: true,
     });
 
     const context = {
       lang,
     };
+
+    if (radiation === RADIATION_MODES.FLUXES) {
+      dispatch({ type: SET_RADIATION_MODE, payload: radiation });
+    }
 
     dispatch({
       type: GET_CONTEXT_SUCCEEDED,
